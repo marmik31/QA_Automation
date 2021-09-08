32ffb4f9905239b04f1fb7055f77a7d04bf18afb
marmik31 <shah.marmik311@gmail.com>
2021-09-08 11:30:24 -0400

    \

---
+++ src/main/java/com/qa/libs/TestBaseClass.java
@@ -0,0 +1,12 @@
+package com.qa.libs;
+
+public class TestBaseClass {
+
+	public void test() {
+		
+		System.out.println("test metod");
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class HomePage {
+
+	public void loginPage()
+	{
+		System.out.println("Home Page");
+		
+	}
+	
+	
+}

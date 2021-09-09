ceda1588d45a447cffb496ff5c5a4dbced877666
marmik31 <shah.marmik311@gmail.com>
2021-09-09 12:07:44 -0400

    new branch commit

---
+++ src/main/java/com/qa/libs/Git_branch_commit_voucher.java
@@ -0,0 +1,10 @@
+package com.qa.libs;
+
+public class Git_branch_commit_voucher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,14 @@
+import org.junit.Test;
 
 public class LoginTest {
 
+@Test
+public void test1() {
+	
+	System.out.println("test1");
+	
+}
+
+	
+	
 }
